81dea3b8a555e15f09f0c20ea3ed50b7782fb8d9
abubkar <abubakaraw8@gmail.com>
2024-04-14 14:26:48 +0500

    sidebar completed

--- src/components/Feed.js
+++ src/components/Feed.js
@@ -1,8 +1,29 @@
 import React from 'react'
+import { useState, useEffect } from 'react';
+import {Box, Stack, Typography} from '@mui/material';
+import { Sidebar, Videos } from './';
 
 const Feed = () => {
   return (
-    <div>Feed</div>
+    <Stack sx={{flexDirection: { sx: "column", md: "row"}}}>
+      <Box sx={{ height: {sx: "auto", md: "92vh"}, borderRight: '3px solid #3d3d3d', px: { sx: 0, md: 2 }}}>
+        <Sidebar />
+
+        <Typography className='copyright' variant='body2' sx={{mt: 1.5, color: '#fff'}}>
+          Copyright 2022 Awanmedia Media
+        </Typography>
+      </Box>
+
+      <Box p={2} sx={{overflow: 'auto', height: '90vh', flex: 2}}>
+        <Typography variant='h4' fontWeight='bold' mb={2} sx={{
+          color: 'white'
+        }}>
+          New <span style={{color: '#F31503'}}>Videos</span>
+        </Typography>
+
+        <Videos videos={[]}/>
+      </Box>
+    </Stack>
   )
 }
 
